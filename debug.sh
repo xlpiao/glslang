54b16db998872b29af4b97f5d32f624cd315c669
Hyangran Park <hyangran_park@tmax.co.kr>
2018-02-09 16:08:17 +0900

    Minor modify

--- debug.sh
+++ debug.sh
@@ -1,3 +1,5 @@
 
 # gdb --args $PWD/dBuild/install/bin/glslangValidator -V -D -S vert test.vs -e main --iy -o test.vs.vert.spv
-gdb --args $PWD/dBuild/install/bin/glslangValidator -V -D -S vert directx.vs -e main --iy -o directx.vs.vert.spv
+# gdb --args $PWD/dBuild/install/bin/glslangValidator -V -D -S vert test.vs -e main --iy -o test.vs.vert.spv -H
+# gdb --args $PWD/dBuild/install/bin/glslangValidator -V -D -S vert directx.vs -e main --iy -o directx.vs.vert.spv
+gdb --args $PWD/dBuild/install/bin/glslangValidator -V -D -S vert directx.vs -e main --iy -o directx.vs.vert.spv -H
